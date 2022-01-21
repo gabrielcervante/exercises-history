d1314ae636ae7e5f2105d8d9891c2a0b53f1ce24
Gabriel Cervante <andersongabriel7377@gmail.com>
2022-01-20 22:18:11 -0300

    Exercises added finished

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,8 +1,16 @@
-package main //handlers
+package handlers
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gabrielcervante/exercises-history/data"
+	"github.com/gin-gonic/gin"
 )
+
+type NewExercises struct {
+	ExerciseName string `json:"exerciseName"`
+	ExerciseTime int    `json:"exerciseTime"`
+}
 
 type Exercise struct {
 }
@@ -11,6 +19,21 @@ func NewExercise() *Exercise {
 	return &Exercise{}
 }
 
-func main() {
-	fmt.Println("vim-go")
+func (e *Exercise) AddExercise(c *gin.Context) {
+
+	var newExercises NewExercises
+
+	c.BindJSON(&newExercises)
+
+	if newExercises.ExerciseName == "" || newExercises.ExerciseTime == 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{
+			"error": "No exercise name or time provided",
+		})
+		return
+	}
+
+	data.AddExercise(newExercises.ExerciseName, newExercises.ExerciseTime)
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"Success": newExercises,
+	})
 }
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gabrielcervante/exercises-history/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	exerciseHandler := handlers.NewExercise()
+
+	router.POST("/", exerciseHandler.AddExercise)
+
+	router.Run()
+}
